0147f37029fc1cca559bd08ff2d709622e3f20da
xavier <xavierperezjimenez4@gmail.com>
2024-10-31 08:38:37 -0500

    1.0.1

--- README.md
+++ README.md
@@ -9,4 +9,32 @@ Condiciones de Reserva:
 
 Este sistema tiene como objetivo facilitar el proceso de reserva, garantizando un registro eficiente de las solicitudes y optimizando la experiencia del cliente.
 
-![](soo.png)
+![""](soo.png)
+
+👉 **Clonar el proyecto desde el repositorio:**
+
+Primero, asegúrate de tener Git instalado en tu sistema. Luego, ejecuta el siguiente comando en tu terminal para clonar el repositorio de tu proyecto Django:
+
+```bash
+git clone git@github.com:xvipj/Hotel-Django.git
+```
+
+👉 **Instalar los paquetes del archivo `requirements.txt`:**
+
+Una vez que hayas clonado el proyecto, navega hasta el directorio del proyecto en tu terminal. Luego, instala los paquetes necesarios utilizando pip y el archivo `requirements.txt`. Ejecuta el siguiente comando:
+
+```bash
+pip install -r requirements.txt
+```
+
+Esto instalará todas las dependencias necesarias para tu proyecto Django.
+
+👉 **Ejecutar el proyecto sin aplicar migraciones:**
+
+Después de instalar los paquetes, puedes ejecutar tu proyecto sin aplicar migraciones. Utiliza el siguiente comando para arrancar el servidor de desarrollo de Django:
+
+```bash
+python manage.py runserver
+```
+
+Esto iniciará el servidor de desarrollo en tu máquina local. Puedes acceder a tu proyecto en un navegador web visitando `http://localhost:8000/`.
--- reservar/views.py
+++ reservar/views.py
@@ -3,25 +3,22 @@ from .forms import ReservaForm
 from datetime import time
 
 def home(request):
+    MAX_NOCHES = 5  # Límite de noches permitido
+
     if request.method == "POST":
         form = ReservaForm(request.POST) 
         if form.is_valid():
-            # Accede a las fechas de entrada y salida
+            # Obtenemos las fechas de entrada y salida del formulario
             fecha_entrada = form.cleaned_data.get('fecha_entrada')
             fecha_salida = form.cleaned_data.get('fecha_salida')
-
-            # Calcula el número de días entre las dos fechas
+            
+            # Calculamos el número de noches y verificamos el límite
             numero_de_dias = (fecha_salida - fecha_entrada).days
-
-            # Calcula el número de noches como un valor positivo
             numero_de_noches = abs(numero_de_dias - 1)
-
-            # Verifica si el número de noches está dentro del límite permitido (5 noches)
-            if numero_de_noches <= 5:
+            if numero_de_noches <= MAX_NOCHES:
                 form.save()  # Guarda la reserva si es válida
                 return redirect('home')
             else:
-                # Contexto cuando el número de noches excede el límite permitido
                 context = {
                     'form': form,
                     'mensaje': "Superas el número de noches permitido. Por favor, comuníquese al +57 3248076520"
@@ -29,5 +26,4 @@ def home(request):
                 return render(request, 'home.html', context)  # Renderiza la página con mensaje de error
     else:
         form = ReservaForm()
-        # Renderiza la página inicial con el formulario
         return render(request, 'home.html', {'form': form})
